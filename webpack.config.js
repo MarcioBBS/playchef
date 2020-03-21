b2123f9e79faa7bdb9b0cb97917f2dbc2e713082
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-03-21 01:31:17 -0700

    Add comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+/**
+ * To configure webpack we need to know the 4 concepts: 1- entry point; 2- the output; 3- loaders; 4- plugins.
+ * 1- Entry point: Where the webpack will start to bundling. It'll look for all the dependecies which it should bundle together.
+ * 2- The output: Where to save the bundled file
+ * 3- Loaders: Loaders in Webpack allow us to import or to load all kinds of different files. And more importantly, to also process them. Like converting SASS to CSS Code or covert ES6 code to ES5 JavaScript.
+*/
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const isDevelopment = process.env.NODE_ENV === 'development';
