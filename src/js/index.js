6b8aa8274e87330a0ab02de7bc70a82b7873ffe4
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-03-22 14:04:39 -0700

    :sparkles: Create loader spinning icon

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import styles from '.././main.scss';
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 ;
 
 // The State pattern provides state-specific logic to a limited set of objects in which each object represents a particular state.
@@ -23,17 +23,20 @@ const controlSearch = async () => {
         state.search = new Search(query);
 
         // 3) Prepare UI for results
-        searchView.clearInput();
-        searchView.clearResults();
+        searchView.clearInput(); // Clear input text
+        searchView.clearResults(); // Clear result list
+        renderLoader(elements.searchRes); // Adds spinning loader icon
 
         // 4) Search for recipes
         await state.search.getResults(); // Await the results until the promise is fulfilled, then it can be render on step 5
         
         // 5) Render results on UI
-        searchView.renderResults(state.search.result);
+        clearLoader(); // Removes spinning loader icon
+        searchView.renderResults(state.search.result); // Render/Shows all result list
     }
 }
 
+// Trigger the result
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
