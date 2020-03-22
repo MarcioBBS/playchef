6b58bdb4175f376b91630b6401c7a05fe7da410d
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-03-21 17:08:32 -0700

    :sparkles: New DOM handler

---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+// Handle all elements from the DOM
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list')
+
+}
