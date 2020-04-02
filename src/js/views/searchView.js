71e70043a5341fff89b2fbd472f2d8d7e94a4170
TheBear <44812306+MarcioBBS@users.noreply.github.com>
2020-04-01 22:20:45 -0700

    :bug:  Fix bug

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -16,7 +16,7 @@ export const highlightSelected = id => {
         el.classList.remove('results__link--active');
     });
 
-    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+    document.querySelector(`.results__link[href="#${id}"]`).classList.add('results__link--active');
 };
 
 /**
@@ -32,7 +32,7 @@ export const highlightSelected = id => {
  * acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', tomato]
  * acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', tomato]
  */
-const reduceTitle = (title, limit = 17) => {
+export const reduceTitle = (title, limit = 17) => {
     const newTitle = [];
 
     if (title.length > limit) {
